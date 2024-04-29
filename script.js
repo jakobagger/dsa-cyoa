5f2bb951e1e1610672de68ad5735ca380faf8ec4
jakobagger <113458342+jakobagger@users.noreply.github.com>
2024-04-29 19:50:40 +0200

    script updated
    most functions to dynamically create text nodes

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-document.addEventListener("load", start);
+document.addEventListener("DOMContentLoaded", start);
 
 function start(){
- console.log("JS running");   
+ console.log("JS running");
+ showScene(firstScene);   
+}
+
+const firstScene = {
+    title: "Første scene",
+    text: /*html*/`<p> Historien begynder </p>`,
+    choices: [
+        "Valg et",
+        "Valg to"
+    ]
+}
+
+function showScene(scene){
+    const html = /*html*/`<div class="scene">
+    <h2>${scene.title}</h2>
+    <div>${scene.text}</div>
+    <div class="choices">
+        ${scene.choices.map((choice, i) => `<button id="btn-choice${i}">${choice}</button>`).join(" ")}
+    </div>
+    </div>
+    `
+
+    document.getElementById("main").insertAdjacentHTML("beforeend", html);
 }
